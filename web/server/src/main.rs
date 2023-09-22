2df785434e1a0b99faff651b56ed488039987cad
jangryga <jangryga@gmail.com>
2023-09-22 22:29:12 +0100

    init CI

--- web/server/src/main.rs
+++ web/server/src/main.rs
@@ -1,4 +1,5 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder, middleware::Logger};
+use env_logger;
 
 #[get("/")]
 async fn hello() -> impl Responder {
@@ -17,8 +18,10 @@ async fn manual_hello() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+
     HttpServer::new(|| { 
-        App::new().service(hello).service(echo).route("/hey", web::get().to(manual_hello))
-    }).bind(("127.0.0.1", 8080))?.run().await
+        App::new().wrap(Logger::new("%a %{User-Agent}i")).service(hello).service(echo).route("/hey", web::get().to(manual_hello))
+    }).bind(("0.0.0.0", 8080))?.run().await
 }
 
