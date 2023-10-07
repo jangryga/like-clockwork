14e78c96c9ef710499bbefbd2d7bda75eebadde0
jangryga <jangryga@gmail.com>
2023-10-07 08:54:09 +0100

    add prisma

--- web/server/src/main.rs
+++ web/server/src/main.rs
@@ -1,27 +1,35 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder, middleware::Logger};
+use actix_web::HttpRequest;
+use actix_web::{get, middleware::Logger, post, web, App, HttpResponse, HttpServer, Responder};
 use env_logger;
+mod db;
 
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world")
+use db::user;
+use db::PrismaClient;
+use prisma_client_rust::NewClientError;
+
+
+async fn hello(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+    format!("Hello {}", &name)
 }
 
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
-
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello from Actix!")
+async fn health_check(req: HttpRequest) -> impl Responder {
+    HttpResponse::Ok()
 }
 
 
-#[actix_web::main]
+#[tokio::main]
 async fn main() -> std::io::Result<()> {
+    let client: Result<PrismaClient, NewClientError> = PrismaClient::_builder().build().await;
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
 
-    HttpServer::new(|| { 
-        App::new().wrap(Logger::new("%a %{User-Agent}i")).service(hello).service(echo).route("/hey", web::get().to(manual_hello))
-    }).bind(("0.0.0.0", 8080))?.run().await
+    HttpServer::new(|| {
+        App::new()
+            .wrap(Logger::new("%a %{User-Agent}i"))
+            .route("/", web::get().to(hello))
+            .route("/health_check", web::get().to(health_check))
+    })
+    .bind(("0.0.0.0", 8080))?
+    .run()
+    .await
 }
-
