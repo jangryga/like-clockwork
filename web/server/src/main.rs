76e574e93c13682f92b83266751d3e843dfd126e
jangryga <jangryga@gmail.com>
2023-10-07 13:32:35 +0100

    remove usage of prisma (keep for migration for now)

--- web/server/src/main.rs
+++ web/server/src/main.rs
@@ -1,12 +1,6 @@
 use actix_web::HttpRequest;
 use actix_web::{get, middleware::Logger, post, web, App, HttpResponse, HttpServer, Responder};
 use env_logger;
-mod db;
-
-use db::user;
-use db::PrismaClient;
-use prisma_client_rust::NewClientError;
-
 
 async fn hello(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("World");
@@ -20,7 +14,6 @@ async fn health_check(req: HttpRequest) -> impl Responder {
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let client: Result<PrismaClient, NewClientError> = PrismaClient::_builder().build().await;
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
 
     HttpServer::new(|| {
