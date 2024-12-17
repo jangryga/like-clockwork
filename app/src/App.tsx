46e65c478a5118fd2cca666299222fca62af634a
jangryga <jangryga@gmail.com>
2024-12-17 10:00:53 +0100

    a basic timer

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,10 +1,31 @@
+// import { invoke } from "@tauri-apps/api/core";
+// async function greet() {
+  // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+  // setGreetMsg(await invoke("greet", { name }));
+// }
+
+import { useEffect, useState } from "react";
+
+
 function App() {
+  const [time, setTime] = useState(new Date)
+  useEffect(() => {
+    const interval = setInterval(() => setTime(new Date), 1000)
+    return () => clearInterval(interval)
+  }, [])
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <main className="bg-[#272525] w-full h-[100vh] flex justify-center items-center text-white">
+      {displayTime(time)}
+    </main>
+  );
 }
 
-export default App
+function displayTime(time: Date): string {
+  const hour = time.getHours();
+  const minute = time.getMinutes()
+  const seconds = time.getSeconds()
+  return `${hour}:${minute}:${seconds >= 10 ? seconds : "0" + seconds}`
+}
+
+export default App;
